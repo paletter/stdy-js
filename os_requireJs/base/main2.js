95af398726fc826c7a9ecefcd3474173cd334c2e
paletter <234866877@qq.com>
2016-10-10 11:40:13 +0800

    EX

--- os_requireJs/base/main2.js
+++ os_requireJs/base/main2.js
@@ -1,5 +1,6 @@
 // 当网页需要加载的JS可能来自本地服务器、其他网站或CDN，就得通过require.config是用来配置模块加载位置
 require.config({
+    baseUrl: "./",
     paths: {
         "jquery": ["http://upcdn.b0.upaiyun.com/libs/jquery/jquery-1.8.1.min"],
         "module_2": "./module_2"
