e777b894c8f5d1f2dd99d0111708b94ba8119460
Seamless Bias <234866877@qq.com>
2016-10-16 01:07:07 +0800

    F

--- os_grunt/requirejs/Gruntfile.js
+++ os_grunt/requirejs/Gruntfile.js
@@ -3,7 +3,14 @@ module.exports = function(grunt){
 
     grunt.initConfig({
         requirejs: {
-
+            compile: {
+                options: {
+                    baseUrl: "./js",
+                    mainConfigFile: "js/main.js", // 路径以 Grunt.js 为根目录
+                    include: [ "main.js" ], // 路径以 baseUrl 为根目录; 加载的js在这配置
+                    out: "dest/main.js"
+                }
+            }
         }
     });
 
